4574981eb9a0bc6722cd594fb4ccd7aa73a92da8
Mathias Lafeldt <misfire@debugon.org>
2011-02-20 13:03:24 +0100

    Solve logpuzzle/logpuzzle.py

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -18,14 +18,32 @@ Here's what a puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
 
+def url_key(url):
+  m = re.search(r'-(\w+)-(\w+)\.\w+', url)
+  if m:
+    return m.group(2)
+  else:
+    return url
+
 
 def read_urls(filename):
   """Returns a list of the puzzle urls from the given log file,
   extracting the hostname from the filename itself.
   Screens out duplicate urls and returns the urls sorted into
   increasing order."""
-  # +++your code here+++
-  
+  host = "http://" + filename[filename.index('_') + 1:]
+
+  f = open(filename, 'r')
+  text = f.read()
+  f.close()
+
+  urls = []
+  for get in re.findall(r'GET (\S+/puzzle/\S+.\w+) HTTP/', text):
+    url = host + get
+    if url not in urls:
+      urls.append(url)
+  return sorted(urls, key=url_key)
+
 
 def download_images(img_urls, dest_dir):
   """Given the urls already in the correct order, downloads
@@ -35,8 +53,19 @@ def download_images(img_urls, dest_dir):
   with an img tag to show each local image file.
   Creates the directory if necessary.
   """
-  # +++your code here+++
-  
+  if not os.path.exists(dest_dir):
+    os.mkdir(dest_dir)
+
+  f = open(os.path.join(dest_dir, 'index.html'), 'w')
+
+  for i, url in enumerate(img_urls):
+    print 'GET', url
+    img = 'img' + str(i)
+    urllib.urlretrieve(url, os.path.join(dest_dir, img))
+    f.write('<img src="' + img + '" />')
+
+  f.close()
+
 
 def main():
   args = sys.argv[1:]
